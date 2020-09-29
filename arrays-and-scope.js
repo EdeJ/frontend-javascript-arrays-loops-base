2619caef2ab182bf96db23f40b14bf60174a71b5
Emiel de Jong <emieldejong@xs4all.nl>
2020-09-29 15:54:11 +0200

    ready

--- arrays-and-scope.js
+++ arrays-and-scope.js
@@ -4,19 +4,35 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // --------------------------
 // Opdracht 1: Check hoeveel studenten hun studie cum laude hebben gehaald (8 of hoger) en log dit in de console.
 // --------------------------
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 8) {
+        console.log(grades[i] + " = Cum laude!");
+    } else {
+        console.log(grades[i] + " =  NIET Cum laude!");
+    }
 
+}
 
 // --------------------------
 // Opdracht 2: Check welke waarde in de array het hoogste is, en log dit in de console
 // --------------------------
-
+let highestNumber = 0;
+for (let i = 0; i < grades.length; i++) {
+    if (grades[i] > highestNumber) {
+        highestNumber = grades[i]
+    }
+}
+console.log("het hoogste nummer is: " + highestNumber)
 
 // --------------------------
 // Opdracht 3: Tel alle waardes van de array bij elkaar op en bereken het gemiddelde. Log dit in de console.
 // --------------------------
-
-
-
+const indexes = grades.length;
+let total = 0;
+for (let i = 0; i < grades.length; i++) {
+    total += grades[i]
+}
+console.log("het gemiddelde =" + total / indexes)
 
 
 
--- arrays.js
+++ arrays.js
@@ -7,6 +7,9 @@ let names = ["Henk", "Piet", "Fred", "Joop"];
 // Zorg ervoor dat er -"je" achter iedere naam wordt geplakt
 // geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] + "je");
+}
 
 
 // --------------------------
@@ -17,7 +20,9 @@ const numbers = [2, 4, 5, 29, 38];
 
 // Vermenigvuldig iedere waarde in de array met 3
 // geeft: [ 6, 12, 15, 87, 114 ];
-
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i] * 3);
+}
 
 
 // --------------------------
@@ -29,4 +34,8 @@ const figures = [4, 6, 12, 34, 1];
 // Tel 3 op bij ieder item in de array
 // geeft: [ 7, 9, 15, 37, 4 ]
 
+for (let i = 0; i < figures.length; i++) {
+    console.log(figures[i] + 3);
+
+}
 
